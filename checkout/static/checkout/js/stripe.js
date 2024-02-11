3450972277cd509d07ca82b0c7abd9f08aace9b7
Hebaabbas <heba1996abbas@gmail.com>
2024-02-11 03:59:59 +0000

    add basic checkout functionality

--- checkout/static/checkout/js/stripe.js
+++ checkout/static/checkout/js/stripe.js
@@ -1,6 +1,6 @@
-var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
-var client_secret = $('#id_client_secret').text().slice(1, -1);
-var stripe = Stripe(stripe_public_key);
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
 var style = {
     base: {
@@ -17,4 +17,63 @@ var style = {
 };
 var card = elements.create('card', {style: style});
 card.mount('#card-element');
+
+
+// React to real-time validation errors on the card element
+card.addEventListener('change', function (event) {
+    var errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        var htmlContent = '<span class="icon" role="alert"><i class="fas fa-times"></i></span><span>' + event.error.message + '</span>';
+        errorDiv.innerHTML = htmlContent;
+    } else {
+        errorDiv.textContent = '';
+    }
+});
+
+// Handle form submit
+var form = document.getElementById('payment-form');
+
+form.addEventListener('submit', async function (ev) {
+    ev.preventDefault();
+
+    // Disable card and submit button during processing
+    card.update({ disabled: true });
+    $('#submit-button').attr('disabled', true);
+
+    try {
+        // Confirm card payment with Stripe
+        const result = await stripe.confirmCardPayment(clientSecret, {
+            payment_method: {
+                card: card,
+            },
+        });
+
+        if (result.error) {
+            // Display error message
+            const errorDiv = document.getElementById('card-errors');
+            const html = `
+                <span class="icon" role="alert">
+                    <i class="fas fa-times"></i>
+                </span>
+                <span>${result.error.message}</span>`;
+            $(errorDiv).html(html);
+
+            // Re-enable card and submit button
+            card.update({ disabled: false });
+            $('#submit-button').attr('disabled', false);
+        } else {
+            // If payment is successful, submit the form
+            if (result.paymentIntent.status === 'succeeded') {
+                form.submit();
+            }
+        }
+    } catch (error) {
+        // Handle unexpected errors
+        console.error('An unexpected error occurred:', error);
+
+        // Re-enable card and submit button
+        card.update({ disabled: false });
+        $('#submit-button').attr('disabled', false);
+    }
+});
 
