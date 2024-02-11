c8f32f33d47cc1453e611133036fb3dea0435d92
Hebaabbas <heba1996abbas@gmail.com>
2024-02-11 09:40:11 +0000

    add class methods to webhook handler and webhook view

--- checkout/urls.py
+++ checkout/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from .webhooks import webhook
+
 
 urlpatterns = [
     path('', views.checkout, name='checkout'),
     path('checkout_done/<order_number>', views.checkout_done, name='checkout_done'),
-
+    path('wh/', webhook, name='webhook'),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
